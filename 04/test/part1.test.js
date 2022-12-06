236daf6a8ee26be0c6828fbf302d728e06b05c0a
Yulia Cech <yulia.cech@elastic.co>
2022-12-06 13:41:52 +0100

    Solve day 4

---
+++ 04/test/part1.test.js
@@ -0,0 +1,7 @@
+const {resolve} = require("path");
+const solve = require('../part1');
+
+const filename = resolve(__dirname, './input.txt');
+test('solves the example input', () => {
+    expect(solve(filename)).toBe(2);
+});
