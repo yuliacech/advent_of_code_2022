09c5de3cdeaf151ce35974d7ee6ce6ec9285e484
Yulia Cech <yulia.cech@elastic.co>
2022-12-06 21:48:37 +0100

    Solve day 6

---
+++ 06/solve.js
@@ -0,0 +1,12 @@
+const solve = (input, chunkLength) => {
+    let index = 0;
+    while (index < input.length - chunkLength) {
+        const chunk = input.slice(index, index + chunkLength);
+        if (new Set(chunk).size === chunkLength) {
+            break;
+        }
+        index++;
+    }
+    return index + chunkLength;
+}
+module.exports = solve;
---
+++ 06/test/part2.test.js
@@ -0,0 +1,15 @@
+const solvePart2 = require('../part2');
+
+describe('day 6 part 2', () => {
+[
+    ['mjqjpqmgbljsphdztnvjfqwrcgsmlb', 19],
+    ['bvwbjplbgvbhsrlpgdmjqwftvncz', 23],
+    ['nppdvjthqldpwncqszvftbrmjlhg', 23],
+    ['nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 29],
+    ['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 26]].map(input => {
+    test('solves the example input', () => {
+        expect(solvePart2(input[0])).toBe(input[1]);
+    });
+});
+});
+
