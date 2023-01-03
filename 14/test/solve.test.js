f6e83d675cfc443a5d8ca96cf8a6fbcff01c849c
Yulia Cech <yulia.cech@elastic.co>
2023-01-03 22:11:58 +0100

    Fix typo

--- 14/test/solve.test.js
+++ 14/test/solve.test.js
@@ -2,10 +2,10 @@ const input = require('./input');
 const part1 = require('../part1');
 const part2 = require('../part2');
 
-test('solves part1', () => {
+test('solves part 1', () => {
   expect(part1(input)).toBe(24);
 });
 
-test('solves part1', () => {
+test('solves part 2', () => {
   expect(part2(input)).toBe(93);
 });
